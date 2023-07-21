c26cf83c1b4f2076b4eb0645ea339030cd714d79
pelaezluis <luispelaezbedoya@gmail.com>
2023-07-21 18:06:01 -0500

    pequeñas correcciones en login y seguridad

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.params import Body
 from fastapi.security import OAuth2PasswordRequestForm
-from app.schemas.user_schema import Token
+from app.schemas.user_schema import LoginData, Token
 from app.seguridad.seguridad_login import authenticate_user, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 from datetime import timedelta
 
@@ -8,6 +9,23 @@ router = APIRouter()
 
 @router.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    print("Access token created:", access_token)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/access", response_model=Token)
+def login_for_access_token(form_data: LoginData = Body(...)):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
